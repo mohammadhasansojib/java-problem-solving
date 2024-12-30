67174fe860807cfc2b941e35f7bcf817b168c96d
sajib hasan <sajibhasan800080@gmail.com>
2024-12-30 10:24:04 +0600

    solved string to array convert problem

---
+++ string-to-array/Array.java
@@ -0,0 +1,9 @@
+class Array{
+  public static void main(String[] args){
+    String myStr = "Hello";
+    char[] arr = myStr.toCharArray();
+
+    System.out.println(arr[0]);
+    //System.out.println(myStr[0]); //invalid
+  }
+}
